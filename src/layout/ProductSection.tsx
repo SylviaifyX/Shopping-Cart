230394f1ef49239f0d56087cabc650ae8ba9fd34
SylviaifyX <ifeomasylviaokpara@gmail.com>
2024-11-10 14:47:59 +0100

    work in progress

--- src/layout/ProductSection.tsx
+++ src/layout/ProductSection.tsx
@@ -6,13 +6,13 @@ import "aos/dist/aos.css";
 import { useEffect } from "react";
 const ProductSection = () =>{
     useEffect(() =>{
-        Aos.init({duration:1000, offset:200, once: true, anchorPlacement: "top-bottom"  });
+        Aos.init({duration:1000, offset:100, once: true, anchorPlacement: "top-bottom"  });
     },[])  
     return(
         <section className="gird items-center">
             <div className="grid  grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-10 ">
-                {dataItem.map((items) =>(
-                    <div className="" key={items.id} data-aos ="fade-up">
+                {dataItem.map((items, index) =>(
+                    <div className="" key={items.id} data-aos ="fade-up"  data-aos-delay={index * 200}>
                         <ProductCard {...items}  quantity={0} subtotal={items.price}/>
                         {/* {JSON.stringify(items)} */}
                     </div>
